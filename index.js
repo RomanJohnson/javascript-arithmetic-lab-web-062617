a5fcabe6d94f33eb22e771f8ed8a5a1464e5f976
github username <github email address>
2017-08-08 09:56:36 -0400

    Done.

---
+++ index.js
@@ -0,0 +1,40 @@
+function add(a, b) {
+  return a+b
+}
+ function subtract(a, b) {
+   return a-b
+ }
+ function multiply(a, b){
+   return a*b
+ }
+function divide(a, b){
+  return a/b
+}
+function inc(n) {
+  var n
+  n++
+  return n
+}
+
+function dec(n){
+  var n
+  n--
+  return n
+}
+
+function makeInt(n) {
+  var n
+  return parseInt(n, 10)
+
+}
+
+// function makeInt(n) {
+//   var n
+//   parseInt(n, 10)
+//   return n
+// }
+function preserveDecimal(n){
+  var n
+  return parseFloat(n)
+
+}
